08026f655552d58f6a7df98d99101a14ff3b3d91
yangyunpeng <yunpeng.yang@ecarxgroup.com>
2022-10-24 17:57:08 +0800

    feat:basic test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include "string"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    char ch[] = "啊";
+    string str = "哈啊哈";
+
+    cout << "Hello, World!" << sizeof ch << endl;
+    cout << "Hello, World!" << str << endl;
+
+    bool flag = false;
+
+    if (flag) {
+        str = "嘿嘿";
+    } else {
+        str = "哈哈";
+    }
+
+    // 键盘输入
+    cin >> str;
+    cout << "input value = " + str << endl;
+
+    for (int i = 1; i < 10; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << j << " x " << i << " = "<< i*j << "\t";
+        }
+        cout << endl;
+    }
     return 0;
 }
